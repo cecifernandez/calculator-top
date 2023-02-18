be9a4bc1173927db28b68a1d72e681566c8572fd
cecifernandez <mceciliafernandezst@gmail.com>
2023-02-18 15:57:45 -0300

    mvp

---
+++ app.js
@@ -0,0 +1,9 @@
+// app should have:
+// add, substract, multiply, divide
+// clear button
+// read pressed buttons and store in variable for use
+// make operation with said buttons
+// store result
+// show everything on screen
+// round big numbers
+// disable . if it's already used
